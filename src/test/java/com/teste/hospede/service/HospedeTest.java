add7a93fcdab6f0ca0b545796647c918fa9929db
Maria Fernanda <132359722+Marifer150805@users.noreply.github.com>
2024-11-07 08:59:18 -0300

    ProjTeste

---
+++ src/test/java/com/teste/hospede/service/HospedeTest.java
@@ -0,0 +1,14 @@
+package com.teste.hospede.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HospedeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
